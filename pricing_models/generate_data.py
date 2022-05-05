b4d8b46d9577821d5b8e8ceaa9c892c86ff1f3b2
paolodelia99 <paolo.delia99@gmail.com>
2022-05-05 16:22:09 +0200

    rewritten binom and trinom pricing model code with numba, runned Model-Testing-BinTri.ipynb

--- pricing_models/generate_data.py
+++ pricing_models/generate_data.py
@@ -4,11 +4,13 @@ from typing import Tuple
 import numpy as np
 import pandas as pd
 
-from pricing_models.bomp import binom_price_tree, binom_option_tree
+from numba import jit, int32, float32, float64
+
+from pricing_models.bomp import binom_price_tree, binom_option_tree_c, binom_option_tree_p
 from pricing_models.bsm import geo_paths
 from pricing_models.heston import generate_heston_path
 from pricing_models.monte_carlo import mc_pricing_american
-from pricing_models.trinomial_tree import topm, t_price_tree, t_option_tree
+from pricing_models.trinomial_tree import t_price_tree, t_option_tree_c, t_option_tree_p
 
 
 def params_ranges(S,
@@ -35,7 +37,8 @@ def params_ranges(S,
     return strikes, vols, interests, taus
 
 
-def generate_binom_option_chain(S, T, r, sigma, type_: str, strike_step:float = 1.0) -> pd.DataFrame:
+@jit(float64[:, :](float32, float32, float32, float32), nopython=True, fastmath=True)
+def generate_binom_option_chain_c(S, T, r, sigma) -> np.array:
     """
     Generate the option chain using the binomial tree model given the price of the underlying at time 0, the time to
     expiration, the interest free rate and the underlying volatility
@@ -44,25 +47,48 @@ def generate_binom_option_chain(S, T, r, sigma, type_: str, strike_step:float = 
     :param T: time to expiration (in years)
     :param r: interest free rate
     :param sigma: underlying volatility
-    :param type_: option's type: 'C' for a call 'P' for a put
-    :param strike_step:
     :return: pandas DataFrame containing the option chain
     """
     n = int(np.floor(365 * T))
-    strikes = np.arange(S // 2, S * 2 + 1, strike_step)
-    option_chain = pd.DataFrame(
-        columns=['Price', 'Strike', 'Type', 'Vol', 'Interest Rate', 'Time to Expiration', 'Option Price'])
+    strikes = np.arange((2 * S) // 3, ((S * 3) // 2) + 1, 1.0)
 
     delta_t = T / n
     up = np.exp(sigma * np.sqrt(delta_t))
     p = (np.exp(r * delta_t) - (1 / up)) / (up - (1 / up))
     price_tree = binom_price_tree(S, n, up)
+    option_chain = np.zeros((len(strikes), 7))
 
-    for strike in strikes:
-        price = binom_option_tree(price_tree, strike, n, delta_t, r, p, type_)[0, 0]
-        option_chain = option_chain.append(
-            {'Price': S, 'Strike': strike, 'Type': type_, 'Vol': sigma, 'Interest Rate': r, 'Time to Expiration': T,
-             'Option Price': price}, ignore_index=True)
+    for i, strike in enumerate(strikes):
+        price = binom_option_tree_c(price_tree, strike, n, delta_t, r, p)[0, 0]
+        option_chain[i] = np.array([S, strike, 1, sigma, r, T, price])
+
+    return option_chain
+
+
+@jit(float64[:, :](float32, float32, float32, float32), nopython=True, fastmath=True)
+def generate_binom_option_chain_p(S, T, r, sigma) -> np.array:
+    """
+    Generate the option chain using the binomial tree model given the price of the underlying at time 0, the time to
+    expiration, the interest free rate and the underlying volatility
+
+    :param S: underlying price at time t=0
+    :param T: time to expiration (in years)
+    :param r: interest free rate
+    :param sigma: underlying volatility
+    :return: pandas DataFrame containing the option chain
+    """
+    n = int(np.floor(365 * T))
+    strikes = np.arange((2 * S) // 3, ((S * 3) // 2) + 1, 1.0)
+
+    delta_t = T / n
+    up = np.exp(sigma * np.sqrt(delta_t))
+    p = (np.exp(r * delta_t) - (1 / up)) / (up - (1 / up))
+    price_tree = binom_price_tree(S, n, up)
+    option_chain = np.zeros((len(strikes), 7))
+
+    for i, strike in enumerate(strikes):
+        price = binom_option_tree_p(price_tree, strike, n, delta_t, r, p)[0, 0]
+        option_chain[i] = np.array([S, strike, 1, sigma, r, T, price])
 
     return option_chain
 
@@ -86,24 +112,33 @@ def binom_option_data(S,
     :param tau_range: (start_vol, end_vol, step_vol) for the volatility range
     :return: pandas DataFrame containing the option data for different ranges  of params
     """
-    strikes, vols, interests, taus = params_ranges(
-        S,
-        strike_step,
-        vol_range,
-        interest_range,
-        tau_range
-    )
-    option_data = pd.DataFrame(
-        columns=['Price', 'Strike', 'Type', 'Vol', 'Interest Rate', 'Time to Expiration', 'Option Price'])
+    chains = []
+    prices = np.arange((S // 4) * 3, (S // 4) * 5, strike_step)
 
-    for sigma, r, T in product(vols, interests, taus):
-        opt_chain = generate_binom_option_chain(S, T, r, sigma, type_, strike_step=1.0)
-        option_data = option_data.append(opt_chain, ignore_index=True)
+    for price in prices:
+        _, vols, rs, taus = params_ranges(
+            price,
+            strike_step=2.5,
+            vol_range=vol_range,
+            interest_range=interest_range,
+            tau_range=tau_range
+        )
+        for v, r, t in product(vols, rs, taus):
+            if type_ == 'C':
+                opt_chain = generate_binom_option_chain_c(price, v, r, t)
+            else:
+                opt_chain = generate_binom_option_chain_p(price, v, r, t)
+            chains.append(opt_chain)
 
+    option_data = pd.DataFrame(np.concatenate(chains),
+                               columns=['Price', 'Strike', 'Type', 'Vol', 'Interest Rate', 'Time to Expiration',
+                                        'Option Price'])
+    option_data['Type'] = type_
     return option_data
 
 
-def generate_trinom_option_chain(S, T, r, sigma, type_: str, strike_step:float = 1.0) -> pd.DataFrame:
+@jit(float64[:, :](float32, float32, float32, float32), nopython=True, fastmath=True)
+def generate_trinom_option_chain_c(S, T, r, sigma) -> pd.DataFrame:
     """
     Generate the option chain using the trinomial tree model given the price of the underlying at time 0, the time to
     expiration, the interest free rate and the underlying volatility
@@ -113,13 +148,10 @@ def generate_trinom_option_chain(S, T, r, sigma, type_: str, strike_step:float =
     :param r: interest free rate
     :param sigma: underlying volatility
     :param type_: option's type: 'C' for a call 'P' for a put
-    :param strike_step:
     :return: pandas DataFrame containing the option chain
     """
     n = int(np.floor(365 * T))
-    strikes = np.arange(S // 2, S + (S // 2) + 1, strike_step)
-    option_chain = pd.DataFrame(
-        columns=['Price', 'Strike', 'Type', 'Vol', 'Interest Rate', 'Time to Expiration', 'Option Price'])
+    strikes = np.arange((2 * S) // 3, ((S * 3) // 2) + 1, 1.0)
 
     delta_t = T / n
     up = np.exp(sigma * np.sqrt(2 * delta_t))
@@ -130,12 +162,45 @@ def generate_trinom_option_chain(S, T, r, sigma, type_: str, strike_step:float =
             np.exp(sigma * np.sqrt(delta_t / 2)) - np.exp(-sigma * np.sqrt(delta_t / 2)))) ** 2
     p_mid = 1 - (p_up + p_down)
     price_tree = t_price_tree(S, n, up, down)
+    option_chain = np.zeros((len(strikes), 7))
 
-    for strike in strikes:
-        price = t_option_tree(price_tree, strike, n, delta_t, r, p_up, p_down, p_mid, type_)[0, 0]
-        option_chain = option_chain.append(
-            {'Price': S, 'Strike': strike, 'Type': type_, 'Vol': sigma, 'Interest Rate': r, 'Time to Expiration': T,
-             'Option Price': price}, ignore_index=True)
+    for i, strike in enumerate(strikes):
+        price = t_option_tree_c(price_tree, strike, n, delta_t, r, p_up, p_down, p_mid)[0, 0]
+        option_chain[i] = np.array([S, strike, 1, sigma, r, T, price])
+
+    return option_chain
+
+
+@jit(float64[:, :](float32, float32, float32, float32), nopython=True, fastmath=True)
+def generate_trinom_option_chain_p(S, T, r, sigma) -> pd.DataFrame:
+    """
+    Generate the option chain using the trinomial tree model given the price of the underlying at time 0, the time to
+    expiration, the interest free rate and the underlying volatility
+
+    :param S: underlying price at time t=0
+    :param T: time to expiration (in years)
+    :param r: interest free rate
+    :param sigma: underlying volatility
+    :param type_: option's type: 'C' for a call 'P' for a put
+    :return: pandas DataFrame containing the option chain
+    """
+    n = int(np.floor(365 * T))
+    strikes = np.arange((2 * S) // 3, ((S * 3) // 2) + 1, 1.0)
+
+    delta_t = T / n
+    up = np.exp(sigma * np.sqrt(2 * delta_t))
+    down = 1 / up
+    p_up = ((np.exp(r * delta_t / 2) - np.exp(-sigma * np.sqrt(delta_t / 2))) / (
+            np.exp(sigma * np.sqrt(delta_t / 2)) - np.exp(-sigma * np.sqrt(delta_t / 2)))) ** 2
+    p_down = ((np.exp(sigma * np.sqrt(delta_t / 2)) - np.exp(r * (delta_t / 2))) / (
+            np.exp(sigma * np.sqrt(delta_t / 2)) - np.exp(-sigma * np.sqrt(delta_t / 2)))) ** 2
+    p_mid = 1 - (p_up + p_down)
+    price_tree = t_price_tree(S, n, up, down)
+    option_chain = np.zeros((len(strikes), 7))
+
+    for i, strike in enumerate(strikes):
+        price = t_option_tree_p(price_tree, strike, n, delta_t, r, p_up, p_down, p_mid)[0, 0]
+        option_chain[i] = np.array([S, strike, 1, sigma, r, T, price])
 
     return option_chain
 
@@ -159,20 +224,28 @@ def trinomial_option_data(S,
     :param tau_range: (start_vol, end_vol, step_vol) for the volatility range
     :return: pandas DataFrame containing the option data for different ranges  of params
     """
-    strikes, vols, interests, taus = params_ranges(
-        S,
-        strike_step,
-        vol_range,
-        interest_range,
-        tau_range
-    )
-    option_data = pd.DataFrame(
-        columns=['Price', 'Strike', 'Type', 'Vol', 'Interest Rate', 'Time to Expiration', 'Option Price'])
+    chains = []
+    prices = np.arange((S // 4) * 3, (S // 4) * 5, strike_step)
 
-    for sigma, r, T in product(vols, interests, taus):
-        opt_chain = generate_trinom_option_chain(S, T, r, sigma, type_, 1.0)
-        option_data = option_data.append(opt_chain, ignore_index=True)
+    for price in prices:
+        _, vols, rs, taus = params_ranges(
+            price,
+            strike_step=2.5,
+            vol_range=vol_range,
+            interest_range=interest_range,
+            tau_range=tau_range
+        )
+        for v, r, t in product(vols, rs, taus):
+            if type_ == 'C':
+                opt_chain = generate_trinom_option_chain_c(price, v, r, t)
+            else:
+                opt_chain = generate_trinom_option_chain_p(price, v, r, t)
+            chains.append(opt_chain)
 
+    option_data = pd.DataFrame(np.concatenate(chains),
+                               columns=['Price', 'Strike', 'Type', 'Vol', 'Interest Rate', 'Time to Expiration',
+                                        'Option Price'])
+    option_data['Type'] = type_
     return option_data
 
 
